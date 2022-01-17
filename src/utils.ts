bb28a9d0087de8c246d5d2bcba16159590c52d31
Анна Калинина <annakalinina@MacBook-Pro-Anna.local>
2022-01-17 16:05:07 +0300

    готовка к тестированию

--- src/utils.ts
+++ src/utils.ts
@@ -1,94 +1,5 @@
 import { FilterPriceRout, guitarType, SortingRout, sortingType } from './const';
 
-// export function getSortRout(activeSorting: {
-//   type: string
-//   order: string
-// }): {
-//   sortPrice: SortingPriceRout
-//   rating: SortingRatingRout
-// } {
-//   switch (JSON.stringify(activeSorting)) {
-//     case JSON.stringify({
-//       type: sortingType.type.price,
-//       order: sortingType.order.increase,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Increase,
-//         rating: SortingRatingRout.Default,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.price,
-//       order: sortingType.order.decrease,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Decrease,
-//         rating: SortingRatingRout.Default,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.price,
-//       order: sortingType.order.default,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Increase,
-//         rating: SortingRatingRout.Default,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.rating,
-//       order: sortingType.order.increase,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Default,
-//         rating: SortingRatingRout.Increase,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.rating,
-//       order: sortingType.order.decrease,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Default,
-//         rating: SortingRatingRout.Decrease,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.rating,
-//       order: sortingType.order.default,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Default,
-//         rating: SortingRatingRout.Increase,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.default,
-//       order: sortingType.order.increase,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Increase,
-//         rating: SortingRatingRout.Default,
-//       };
-//     case JSON.stringify({
-//       type: sortingType.type.default,
-//       order: sortingType.order.decrease,
-//     }):
-//       return {
-//         sortPrice: SortingPriceRout.Decrease,
-//         rating: SortingRatingRout.Default,
-//       };
-//     default:
-//       return {
-//         sortPrice: SortingPriceRout.Default,
-//         rating: SortingRatingRout.Default,
-//       };
-//   }
-// }
-
-// export function getTypeRout (activeType: string[]): string {
-//   if (activeType.length) {
-//     const typeRout:string[] = [];
-//     activeType.map((type) => typeRout.push(Object(FilterTypeGuitarRout)[type]));
-//     return typeRout.join('');
-//   }
-//   return '';
-// }
-
 export function getStringsFromType (type: string): string[] {
   switch (type) {
     case guitarType.acoustic:
@@ -123,13 +34,13 @@ export function getNewParams (activeSorting: {type: string, order: string}, acti
     params.set(FilterPriceRout.to, activeMaxPrice);
   }
   if (activeGuitarTypes.length) {
-    activeGuitarTypes.map((type)=>params.append('type', type));
+    activeGuitarTypes.forEach((type)=>params.append('type', type));
   }
   if (activePage !== 1 && activePage) {
     params.set('page_', String(activePage));
   }
   if (activeStrings && activeStrings.length) {
-    activeStrings.map((item)=>params.append('strings', item));
+    activeStrings.forEach((item)=>params.append('strings', item));
   }
   return params;
 }
